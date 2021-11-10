45eeaf2aa6b0918a4487ca8a2838507eefa7e842
danilokovacs <danilo.kovacs@bandtec.com.br>
2021-11-09 21:40:53 -0300

    Estilização de tela de login

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -8,7 +8,7 @@ function validarSessao() {
     var h1LoginUsuario = document.getElementById("h1_login_usuario");
 
     if (email != null && nome != null) {
-        window.alert(`Seja bem-vindo, ${nome}!`);
+        // window.alert(`Seja bem-vindo, ${nome}!`);
         if (h1LoginUsuario != undefined) {
             h1LoginUsuario.innerHTML = email;
         }
